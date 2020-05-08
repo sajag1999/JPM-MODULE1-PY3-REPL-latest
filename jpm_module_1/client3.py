f28d5c3c76aced2458ea1ff0812885ec947e18a6
sajag chauhan <sajagchauhan7@gmail.com>
2020-05-08 10:48:41 +0000

    Initial commit

---
+++ jpm_module_1/client3.py
@@ -0,0 +1,26 @@
+import urllib2
+import time
+import json
+import random
+# Server API URLs
+QUERY = "http://localhost:8080/query?id={}"
+N = 500
+def getDataPoint(quote):
+  stock = quote['stock']
+  bid_price = float(quote['top_bid']['price'])
+  ask_price = float(quote['top_ask']['price'])
+  price = (bid_price + ask_price)/2
+  return stock, bid_price, ask_price, price
+def getRatio(price_a,price_b):
+  if (price_b == 0):
+    return
+  return price_a/price_b
+if __name__ == "__main__":
+  for _ in range(N):
+    quotes = json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
+    prices = {}
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quotes)
+      prices[stock] = price
+      print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price)
+    print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
